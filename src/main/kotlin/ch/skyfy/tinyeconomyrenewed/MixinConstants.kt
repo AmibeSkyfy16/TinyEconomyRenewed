3b41f1fe87c8b1cb8a04bdfc9c14bd9532c6fb80
AmibeSkyfy16 <colin.viatte@gmail.com>
2022-07-19 13:11:29 +0200

    Everything is working ZERO bugs known for now

--- src/main/kotlin/ch/skyfy/tinyeconomyrenewed/MixinConstants.kt
+++
@@ -1,9 +0,0 @@
-package ch.skyfy.tinyeconomyrenewed;
-
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.inventory.Inventory
-
-object MixinConstants {
-    @JvmField
-    val OPENED_INVENTORIES: Map<Inventory, List<PlayerEntity>> = HashMap()
-}
