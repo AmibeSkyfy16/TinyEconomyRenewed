39bdbe8f7883612bb5c295ad3eec95545432087f
skyfy16 <colin.viatte@gmail.com>
2023-04-17 11:26:29 +0200

    it should be good now

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,4 +9,4 @@ pluginManagement {
 
 //rootProject.buildFileName = "build_01.gradle.kts"
 //rootProject.buildFileName = "shadow2-build.gradle.kts"
-//rootProject.buildFileName = "shadow3-build.gradle.kts"
+rootProject.buildFileName = "shadow-good-build.gradle.kts"
--- shadow-good-build.gradle.kts
+++ shadow-good-build.gradle.kts
@@ -1,6 +1,6 @@
 /*
  *
- * shadow3-build.gradle.kts is based on this : https://github.com/QuiltServerTools/Ledger/blob/master/build.gradle.kts
+ * shadow-good-build.gradle.kts is based on this : https://github.com/QuiltServerTools/Ledger/blob/master/build.gradle.kts
  *
  * This is one of the ways I tried to include no-mod libraries.
  *
@@ -12,6 +12,7 @@
 
 @file:Suppress("GradlePackageVersionRange")
 
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import net.fabricmc.loom.task.RemapJarTask
 
 plugins {
@@ -61,12 +62,9 @@ dependencies {
 
 //    handleIncludes(project, transitiveInclude)
 
-    shadow(implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2"){
-
-    })
+    shadow(implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")!!)
     shadow("org.ktorm:ktorm-core:3.6.0")
     shadow("org.ktorm:ktorm-support-mysql:3.6.0")
-//    shadow("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     shadow("net.lingala.zip4j:zip4j:2.11.2")
     shadow("ch.skyfy.jsonconfiglib:json-config-lib:3.0.14")
     shadow("com.jayway.jsonpath:json-path:2.7.0")
@@ -179,12 +177,12 @@ tasks {
         from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } }
     }
 
-    remapJar{
-        dependsOn(shadowJar.get())
-        inputFile.set(shadowJar.get().archiveFile)
-    }
-
-    shadowJar {
+    /**
+     * thanks to
+     * - https://github.com/QuiltServerTools/Ledger/blob/master/build.gradle.kts
+     * - https://github.com/LuckPerms/LuckPerms/blob/4068c71d5a1ff9f70c59ea22a4abff68604183a3/fabric/build.gradle#L4
+     */
+    named<ShadowJar>("shadowJar"){
         from("LICENSE")
 
         archiveFileName.set("${project.properties["archives_name"]}-${project.properties["mod_version"]}-shadowed.jar")
@@ -197,19 +195,27 @@ tasks {
         exclude("net/kyori/**")
         exclude("org/slf4j/**")
 
-//        dependencies {
-//            exclude("net.fabricmc:.*")
-//
-//            include(dependency("ch.skyfy.tinyeconomyrenewed:.*"))
-//
-//            // We don't want to include the mappings in the jar do we?
-//            exclude("/mappings/*")
-//        }
-
         relocate("org.mariadb.jdbc", "ch.skyfy.tinyeconomyrenewed.libs.jdbc")
         relocate("org.ktorm", "ch.skyfy.tinyeconomyrenewed.libs.ktorm")
         relocate("org.objectweb", "ch.skyfy.tinyeconomyrenewed.libs.objectweb")
+        relocate("org.apache.commons", "ch.skyfy.tinyeconomyrenewed.libs.org.apache.commons")
+        relocate("org.json", "ch.skyfy.tinyeconomyrenewed.libs.org.json")
+        relocate("net.lingala.zip4j", "ch.skyfy.tinyeconomyrenewed.libs.net.lingala.zip4j")
+        relocate("net.minidev", "ch.skyfy.tinyeconomyrenewed.libs.net.minidev")
+        relocate("com.jayway.jsonpath", "ch.skyfy.tinyeconomyrenewed.libs.com.jayway.jsonpath")
+        relocate("com.binance", "ch.skyfy.tinyeconomyrenewed.libs.com.binance")
+        relocate("com.sun.jna", "ch.skyfy.tinyeconomyrenewed.libs.com.sun.jna")
+        relocate("com.google", "ch.skyfy.tinyeconomyrenewed.libs.com.google")
+        relocate("com.github", "ch.skyfy.tinyeconomyrenewed.libs.com.github")
+        relocate("mu", "ch.skyfy.tinyeconomyrenewed.libs.mu")
+        relocate("waffle", "ch.skyfy.tinyeconomyrenewed.libs.waffle")
+        relocate("okio", "ch.skyfy.tinyeconomyrenewed.libs.okio")
+        relocate("okhttp3", "ch.skyfy.tinyeconomyrenewed.libs.okhttp3")
+    }
 
+    named<RemapJarTask>("remapJar"){
+        dependsOn(shadowJar.get())
+        inputFile.set(shadowJar.get().archiveFile)
     }
 
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
